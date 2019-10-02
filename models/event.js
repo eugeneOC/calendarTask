90d79c7c7677d6b4600921d9af2e0697092824e7
Eugene Sim <eugene@ocdigitalnetwork.com>
2019-10-02 12:02:55 +0800

    event dummy data

--- models/event.js
+++ models/event.js
@@ -1,8 +1,46 @@
-'use strict';
-
-module.exports = function(Event) {
-  
-
+const event = {
+  id: "5d8c280e68ae9780ede7a972",
+  //event description
+  title: "This is an event",
+  desc: "Description for an event",
+  start: "2019-09-26T02:14:39.144Z",
+  end: "2019-09-26T02:14:39.144Z",
+  location: "123 Location Address",
+  labelColor: "#ff9800",
+  allDay: false,
+  // Recurrance rule
+  isRecurring: false,
+  recurringRule: "string",
+  // Creator, modifier, owner info
+  createdAt: "2019-09-26T02:53:02.994Z",
+  updatedAt: "2019-09-26T02:53:02.994Z",
+  createdBy: "5d71c2c01f75bda5df726f6a",
+  updatedBy: "5d71c2c01f75bda5df726f6a",
+  userId: "5d71c2c01f75bda5df726f6a",
+  userInfo: {
+    id: "5d71c2c01f75bda5df726f6a",
+    name: "Eugene Sim"
+  },
+  creatorInfo: {
+    id: "5d71c2c01f75bda5df726f6a",
+    name: "Eugene Sim"
+  },
+  updaterInfo: {
+    id: "5d71c2c01f75bda5df726f6a",
+    name: "Eugene Sim"
+  },
+  // Related to
+  eventableId: "5d8c60215ee5c40b34611250",
+  eventableType: "Lead",
+  // Attendees
+  participants: [
+    {
+      rsvp: "no",
+      id: "5d8c280f68ae9780ede7a973",
+      eventId: "5d8c280e68ae9780ede7a972",
+      participantsType: "Lead",
+      participantsId: "5d71c3361f75bda5df7270c3",
+      name: "Lead One"
+    }
+  ]
 };
-
-
